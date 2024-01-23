4e63ef789040e126a5bde58ea2a724e8f674568d
medasravanthi <150466318+medasravanthi@users.noreply.github.com>
2024-01-23 22:18:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import boto3
 
-    client = boto3.client('ec2',region_name='us-west-2') ,
-    response = client.run_instances(
-    ImageId='ami-035bf26fb18e75d1b',
-    MaxCount=1,
-    MinCount=1,
-    InstanceType='t2.micro' ,
-    KeyName = 'i3'
+client = boto3.client('ec2',region_name='us-west-2') ,
+response = client.run_instances(
+  ImageId='ami-035bf26fb18e75d1b',
+  MaxCount=1,
+  MinCount=1,
+  InstanceType='t2.micro' ,
+  KeyName = 'i3'
 
 )
