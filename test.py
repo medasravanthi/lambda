2aa3f817079eac8640d31cdca178044e86911cf5
medasravanthi <150466318+medasravanthi@users.noreply.github.com>
2024-01-23 22:12:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-import json
 import boto3
 
-def lambda_handler(event, context):
     client = boto3.client('ec2',region_name='us-west-2') ,
     response = client.run_instances(
     ImageId='ami-035bf26fb18e75d1b',
